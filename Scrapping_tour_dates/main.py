f0c96c086bc263397980bd707c38bd53266c626e
Márcio Feitosa <marcfeitosa@yahoo.com.br>
2024-04-04 20:43:12 -0300

    Update main & load DB
    Main file was updated in order to mirror the SQL database upgrade I've made.

--- Scrapping_tour_dates/main.py
+++ Scrapping_tour_dates/main.py
@@ -3,10 +3,19 @@ import selectorlib
 import smtplib, ssl
 import os
 import time
+import sqlite3
+
+"INSERT INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
+"SELECT * FROM events WHERE date='2088.10.15'"
+"DELETE FROM events WHERE band='Tigers'"
+
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -37,13 +46,21 @@ def send_email(message):
 
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor.execute("SELECT * FROM events WHERE band=? and city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 # you may need to install a certification in Python, so it can run it. If you see the error:
 # ssl.SSLCertificationError: [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certification
@@ -53,9 +70,10 @@ if __name__ == "__main__":
         extracted = extract(scraped)
         print(extracted)
 
-        content = read(extracted)
+
         if extracted != "No upcoming tours":
-            if extracted not in "data.txt":
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey, new event was found!")
         time.sleep(2)
